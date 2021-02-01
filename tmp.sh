31fc12dff5af0d1271e98f8d6249d47dc84a0309
jueqi wang <x2019cwn@stfx.ca>
2021-02-01 17:57:36 -0400

    tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -7,5 +7,8 @@
 #SBATCH --mail-user=x2019cwn@stfx.ca # used to send emailS
 #SBATCH --mail-type=ALL
 
+module load python/3.6 cuda cudnn gcc/8.3.0
+source /home/jueqi/projects/def-jlevman/jueqi/ENV/bin/activate && echo "$(date +"%T"):  Activated python virtualenv"
+
 LOG_DIR=/home/jueqi/projects/def-jlevman/jueqi/rUnet_log
 tensorboard --logdir="$LOG_DIR" --host 0.0.0.0
