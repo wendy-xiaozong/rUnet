1f3897c459962e29922963370382f9aa15be0a5e
jueqi wang <x2019cwn@stfx.ca>
2021-02-06 20:00:16 -0400

    bugfix

--- project/utils/visualize.py
+++ project/utils/visualize.py
@@ -257,5 +257,5 @@ def log_all_info(
     brainSlice = BrainSlices(module, img, target, preb)
     fig = brainSlice.plot()
 
-    fig.savefig("/home/jueqi/projects/def-jlevman/jueqi/rUnet/3/after scaled.png")
-    # brainSlice.log(state, fig, loss, batch_idx)
+    # fig.savefig("/home/jueqi/projects/def-jlevman/jueqi/rUnet/3/after scaled.png")
+    brainSlice.log(state, fig, loss, batch_idx)
