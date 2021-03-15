2ad9e12d6e01c8af2fa296ba85c7c5daa62e7564
jueqi wang <x2019cwn@stfx.ca>
2021-03-15 15:10:57 -0300

    5

--- run_Cedar_t1t2.sh
+++ run_Cedar_t1t2.sh
@@ -41,11 +41,11 @@ TASK=t1t2  # diffusion
 ACTIVATION=LeakyReLU # LeakyReLU ReLU
 NORMALIZATION=Batch # Batch Group InstanceNorm3d
 WEIGHT_DECAY=1e-8
-IN_CHANNELS=1
-CLIP_MIN=2
-CLIP_MAX=5
-X_image=t1
-y_image=t2
+IN_CHANNELS=2
+CLIP_MIN=4
+CLIP_MAX=3
+X_image=t2
+y_image=t1
 LEARNING_RATE=1e-5
 
 LOG_DIR=/home/jueqi/projects/def-jlevman/jueqi/rUnet_final_result
@@ -53,7 +53,7 @@ LOG_DIR=/home/jueqi/projects/def-jlevman/jueqi/rUnet_final_result
 # run script
 echo -e '\n\n\n'
 echo "$(date +"%T"):  start running model!"
-tensorboard --logdir="$LOG_DIR" --host 0.0.0.0 & python3 /home/jueqi/projects/def-jlevman/jueqi/rUnet/2/project/main.py \
+tensorboard --logdir="$LOG_DIR" --host 0.0.0.0 & python3 /home/jueqi/projects/def-jlevman/jueqi/rUnet/5/project/main.py \
        --gpus=$GPUS \
        --in_channels=$IN_CHANNELS \
        --loss="$LOSS" \
@@ -64,6 +64,7 @@ tensorboard --logdir="$LOG_DIR" --host 0.0.0.0 & python3 /home/jueqi/projects/de
        --normalization="$NORMALIZATION" \
        --batch_size=$BATCH_SIZE \
        --X_image="$X_image" \
+       --use_flair \
        --y_image="$y_image" \
        --task="$TASK" \
        --learning_rate=$LEARNING_RATE \
